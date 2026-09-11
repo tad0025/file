79ec606f6276fda5bcd2674f6c186f55d1b767b2
Thành Đạt <thanhdat050625@gmail.com>
2026-09-11 22:45:07 +0700

    feat: add WebHandler for login, index, and video watch pages

--- internal/handler/web.go
+++ internal/handler/web.go
@@ -104,7 +104,7 @@ func (h *WebHandler) HandleWatchPage(w http.ResponseWriter, r *http.Request) {
 	}
 
 	h.renderTemplate(w, "watch.html", map[string]interface{}{
-		"Title": "Xem:1 " + video.Title,
+		"Title": "Xem: " + video.Title,
 		"Video": video,
 		"Token": token,
 	})
